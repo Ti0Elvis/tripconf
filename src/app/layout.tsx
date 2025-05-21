b8880b581d195facf2b34edbb9f101bab1baeede
Ti0Elvis <elvis.veramijares@gmail.com>
2025-05-21 20:19:46 +0100

    Add UI components: Dropdown Menu: Add theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
+import { ThemeProvider } from "@/providers/theme-provider";
+import { MaxWidthWrapper } from "@/components/max-width-wrapper";
 
 const roboto = Roboto({ weight: ["500", "700", "900"], subsets: ["latin"] });
 
@@ -18,9 +20,15 @@ export default function Layout({ children }: Readonly<Props>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={roboto.className}>
-        <main className="w-full min-h-[calc(100vh-4rem)] py-8 tracking-tight whitespace-pre-wrap break-words">
-          {children}
-        </main>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange>
+          <main className="w-full min-h-[calc(100vh-4rem)] py-8 tracking-tight whitespace-pre-wrap break-words">
+            <MaxWidthWrapper>{children}</MaxWidthWrapper>
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
