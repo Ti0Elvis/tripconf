f55e5260c159b52f069e5b7ff373c4596b25e0cc
Ti0Elvis <elvis.veramijares@gmail.com>
2025-05-22 11:16:57 +0100

    Enhance Header component with dynamic menu for authenticated users; add logout functionality and route links

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,9 +1,33 @@
+"use client";
 import Link from "next/link";
-import { Sheet } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { useContext } from "react";
+import { useRouter } from "next/navigation";
+import { AuthContext } from "@/context/auth";
+import { Button } from "@/components/ui/button";
+import { AlignJustifyIcon } from "lucide-react";
 import { ModeToggle } from "@/components/mode-toggle";
 import { MaxWidthWrapper } from "@/components/max-width-wrapper";
 
+const ROUTES = [
+  { name: "Preventives", path: "/preventives" },
+  { name: "Meals", path: "/meals" },
+  { name: "Services", path: "/services" },
+  { name: "Service Categories", path: "/service-categories" },
+];
+
 export function Header() {
+  const { isLogin, setIsLogin } = useContext(AuthContext)!;
+
+  const { push } = useRouter();
+
   return (
     <Sheet>
       <header className="w-full h-16 sticky top-0 shadow-sm z-50 border-b bg-background">
@@ -12,10 +36,53 @@ export function Header() {
             <span className="font-bold text-primary text-2xl">Tripconf</span>
           </Link>
           <div className="flex items-center gap-2">
+            {isLogin === true && (
+              <SheetTrigger asChild>
+                <Button size="icon" variant="outline">
+                  <AlignJustifyIcon />
+                </Button>
+              </SheetTrigger>
+            )}
             <ModeToggle />
           </div>
         </MaxWidthWrapper>
       </header>
+      {isLogin === true && (
+        <SheetContent className="py-12 px-4">
+          <SheetTitle>Menu</SheetTitle>
+          <SheetDescription className="hidden" />
+          <div className="py-4 flex-grow flex flex-col justify-between">
+            <section className="flex flex-col gap-4">
+              <SheetClose asChild>
+                <Button
+                  variant="outline"
+                  className="w-full"
+                  onClick={() => {
+                    push("/sign-in");
+                    setIsLogin(false);
+                  }}>
+                  Logout
+                </Button>
+              </SheetClose>
+              <hr />
+              {ROUTES.map((route) => (
+                <SheetClose key={route.path} asChild>
+                  <Link href={route.path} prefetch>
+                    <Button variant="outline" className="w-full">
+                      {route.name}
+                    </Button>
+                  </Link>
+                </SheetClose>
+              ))}
+            </section>
+            <SheetClose asChild>
+              <Link href="/preventives/create">
+                <Button className="w-full">Create your preventive</Button>
+              </Link>
+            </SheetClose>
+          </div>
+        </SheetContent>
+      )}
     </Sheet>
   );
 }
